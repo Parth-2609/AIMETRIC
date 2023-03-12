c398f1aa2d76bc8e5f28ee9f1a458caf38fb6803
Parth Barara <parthbarara10@gmail.com>
2023-03-13 04:46:17 +0530

    Added file for footer

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,8 @@ const About = () => {
   return (
     <Box>
         <Typography variant='h1' textAlign='center'>
-            This is the About Page
+            This is the About Page!
+            We have to write about our website in this page.
         </Typography>
     </Box>
   )
